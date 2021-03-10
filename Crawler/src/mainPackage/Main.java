ecd379e15d9bc39ef2227afae479cd0fa24206d5
iquintann <43422021+iquintann@users.noreply.github.com>
2021-03-10 18:47:54 +0100

    Ordenacion del ranking sobre el término introducido

--- Crawler/src/mainPackage/Main.java
+++ Crawler/src/mainPackage/Main.java
@@ -14,9 +14,6 @@ import java.util.Scanner;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
-
-
-
 public class Main {
 
 	public static void main(String[] args) throws Exception {
@@ -30,90 +27,94 @@ public class Main {
 			return;
 		}
 
-		
-
 		Lister lister = new Lister();
-		
-		//Definimos un arraylista para los ficheros que vamos a encontrar
+
+		// Definimos un arraylista para los ficheros que vamos a encontrar
 		ArrayList<String> fileList = new ArrayList<String>();
-		
-		//Definimos una cola para los directorios que vamos a navegar
+
+		// Definimos una cola para los directorios que vamos a navegar
 		Queue<String> directoryQueue = new LinkedList<String>();
 		directoryQueue.add(path);
-	
-		//Cargamos el stopwords
+
+		// Cargamos el stopwords
 		ThesaurusManagement tesaurus = new ThesaurusManagement();
-		
-		
-		//Creamos un objeto que nos ayudar� a llamar los m�todos de contar las palabras de un fichero
-		FichContPalabras wc = new FichContPalabras(fileOut,tesaurus.getStopWords());
-		
-		//Instanciamos la memoria Fat, indice invertido
+
+		// Creamos un objeto que nos ayudar� a llamar los m�todos de contar las palabras
+		// de un fichero
+		FichContPalabras wc = new FichContPalabras(fileOut, tesaurus.getStopWords());
+
+		// Instanciamos la memoria Fat, indice invertido
 		Fat fat = new Fat();
 
 		System.out.println("Cargando ficheros-0%");
-		//Mientras el directorio este con ficheros, lo recorremos y lo a�adimos a un listado
-		while(!directoryQueue.isEmpty()) {
-			
-			//System.out.println("\n\n-->Obteniendo fichero del directorio " + directoryQueue.peek() + "...\n");
+		// Mientras el directorio este con ficheros, lo recorremos y lo a�adimos a un
+		// listado
+		while (!directoryQueue.isEmpty()) {
+
+			// System.out.println("\n\n-->Obteniendo fichero del directorio " +
+			// directoryQueue.peek() + "...\n");
 
 			lister.listingFilesAndDirectories(directoryQueue.remove(), fileList, directoryQueue);
-			
-			//System.out.println("\n-->Lista de directorios " + directoryQueue + "\n");
 
+			// System.out.println("\n-->Lista de directorios " + directoryQueue + "\n");
 
-			//System.out.println("\n	->Leyendo ficheros... \n");
-			//Una vez obtenido todos los ficheros del directorio dado, procesaremos la lista de los mismos
+			// System.out.println("\n ->Leyendo ficheros... \n");
+			// Una vez obtenido todos los ficheros del directorio dado, procesaremos la
+			// lista de los mismos
 			for (int j = 0; j < fileList.size(); j++) {
-				//System.out.println("		Fichero " + fileList.get(j) + ".");
-				//procedemos a analizar fichero por fichero
-				wc.wordCount(fileList.get(j),fat);
-				
+				// System.out.println(" Fichero " + fileList.get(j) + ".");
+				// procedemos a analizar fichero por fichero
+				wc.wordCount(fileList.get(j), fat);
+
 			}
-			
+
 			fileList.clear();
 
 		}
 		System.out.println("Cargando ficheros-100%");
-		
+
 		Map<String, Ocurrencia> mapOcurrencia = wc.getMapOcurrencia();
-		//mostrarTerminosUrls(mapOcurrencia,fat);
-		//lee pasamos una palabra, la buscamos en el tree y nos devuelve las URLs y un ranking
-		
-	    creacionRanking(mapOcurrencia,fat);
-		
-		//fat.mostrarFat();
+		// mostrarTerminosUrls(mapOcurrencia,fat);
+		// lee pasamos una palabra, la buscamos en el tree y nos devuelve las URLs y un
+		// ranking
+
+		creacionRanking(mapOcurrencia, fat);
+
+		// fat.mostrarFat();
 		wc.writeFileOut();
 
 	}
-	
-	
-	
-	
 
 	private static void creacionRanking(Map<String, Ocurrencia> mapOcurrencia, Fat fat) {
-		
+
 		System.out.println("=============================================");
 		System.out.print("Introduzca una palabra para buscar: ");
-		
-		Scanner in = new Scanner(System.in); 
-        String palabraBuscar = in.nextLine();
-		System.out.println("=============================================\n"
-				+ "Mostrando donde aparece la palabra '"+ palabraBuscar+"'\n=============================================");
-		
-		if(mapOcurrencia.containsKey(palabraBuscar)) {
+
+		Scanner in = new Scanner(System.in);
+		String palabraBuscar = in.nextLine();
+		System.out.println("=============================================\n" + "Mostrando donde aparece la palabra '"
+				+ palabraBuscar + "'\n=============================================");
+
+		if (mapOcurrencia.containsKey(palabraBuscar)) {
 			Ocurrencia ocurrenciaPalabra = mapOcurrencia.get(palabraBuscar);
 			Map<Integer, Integer> map = ocurrenciaPalabra.getArbolUrls();
-			for(Entry<Integer, Integer> entry : map.entrySet()) {
+			
+			//Utilizado para ordenar el Map
+			ValueComparator bvc = new ValueComparator(map);
+			TreeMap<Integer, Integer> sorted_map = new TreeMap<Integer, Integer>(bvc);
+			sorted_map.putAll(map);
+
+			for (Entry<Integer, Integer> entry : sorted_map.entrySet()) {
 				Integer key = entry.getKey();
 				Integer value = entry.getValue();
-				String url=fat.devolverUrl(key);
-				System.out.println("Apariciones "+value+" URL: "+url);
-				
+				String url = fat.devolverUrl(key);
+				System.out.println("Apariciones " + value + " URL: " + url);
+
 			}
-			
-			//Map<String, Integer> treeOrdenado = (Map<String, Integer>) ocurrenciaPalabra.entriesSortedByValues(ocurrenciaPalabra.getArbolUrls());
-			//ocurrenciaPalabra.mostrarURLs();
+
+			// Map<String, Integer> treeOrdenado = (Map<String, Integer>)
+			// ocurrenciaPalabra.entriesSortedByValues(ocurrenciaPalabra.getArbolUrls());
+			// ocurrenciaPalabra.mostrarURLs();
 //			Map<Integer, Integer> arbolOrdenar = ocurrenciaPalabra.getArbolUrls();
 //			
 //			
@@ -135,21 +136,42 @@ public class Main {
 //				
 //				
 //			}
-		}else {
+		} else {
 			System.err.println("No se han encontrado coincidencias");
 		}
-		
+
 	}
+
+	
 
 	private static void mostrarTerminosUrls(Map<String, Ocurrencia> map, Fat fat) {
-		for(Entry<String, Ocurrencia> entry : map.entrySet()) {
-		  String key = entry.getKey();
-		  Ocurrencia value = entry.getValue();
+		for (Entry<String, Ocurrencia> entry : map.entrySet()) {
+			String key = entry.getKey();
+			Ocurrencia value = entry.getValue();
 
-		  System.out.print("________________________________________\n\n"+key+" ");
-		  value.mostrarURLs(fat);
+			System.out.print("________________________________________\n\n" + key + " ");
+			value.mostrarURLs(fat);
 		}
-		
+
 	}
 
+}
+
+//Utilizado para ordenar el Map
+class ValueComparator implements Comparator<Integer> {
+	Map<Integer, Integer> base;
+
+	public ValueComparator(Map<Integer, Integer> base) {
+		this.base = base;
+	}
+
+	// Note: this comparator imposes orderings that are inconsistent with
+	// equals.
+	public int compare(Integer a, Integer b) {
+		if (base.get(a) >= base.get(b)) {
+			return -1;
+		} else {
+			return 1;
+		} // returning 0 would merge keys
+	}
 }
